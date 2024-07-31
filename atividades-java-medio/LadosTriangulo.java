fd885a0588f7d9b7d88ccef70da4927dd5a10589
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-07-31 08:37:04 -0300

    Criando Exercicios medio java

---
+++ atividades-java-medio/LadosTriangulo.java
@@ -0,0 +1,20 @@
+/*
+ Implemente um programa em Java que leia as medidas dos lados de um triângulo e
+escreva se ele é equilátero, isósceles ou escaleno. Sendo que:
+ Triângulo Equilátero: possui os 3 lados iguais.
+ Triângulo Isósceles: possui 2 lados iguais.
+ Triângulo Escaleno: possui 3 lados diferentes
+ */
+
+import java.util.Scanner;
+
+public class LadosTriangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        
+
+        
+    }
+}
