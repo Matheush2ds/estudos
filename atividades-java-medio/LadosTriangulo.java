dadacadc9b3ca7a2df16022ad07660384d50bfa1
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-08-01 13:59:02 -0300

    Exercicio Lados Triangulo

--- atividades-java-medio/LadosTriangulo.java
+++ atividades-java-medio/LadosTriangulo.java
@@ -6,15 +6,37 @@ escreva se ele é equilátero, isósceles ou escaleno. Sendo que:
  Triângulo Escaleno: possui 3 lados diferentes
  */
 
-import java.util.Scanner;
+ import java.util.Scanner;
 
 public class LadosTriangulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
-        
+        System.out.println("Digite a medida do lado A: ");
+        double ladoA = scanner.nextDouble();
 
+        System.out.println("Digite a medida do lado B: ");
+        double ladoB = scanner.nextDouble();
+
+        System.out.println("Digite a medida do lado C: ");
+        double ladoC = scanner.nextDouble();
+
+        if (isTrianguloValido(ladoA, ladoB, ladoC)) {
+            if (ladoA == ladoB && ladoB == ladoC) {
+                System.out.println("Triângulo equilátero");
+            } else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC) {
+                System.out.println("Triângulo isósceles");
+            } else {
+                System.out.println("Triângulo escaleno");
+            }
+        } else {
+            System.out.println("As medidas não formam um triângulo válido.");
+        }
         
+        scanner.close();
+    }
+
+    private static boolean isTrianguloValido(double ladoA, double ladoB, double ladoC) {
+        return (ladoA + ladoB > ladoC) && (ladoA + ladoC > ladoB) && (ladoB + ladoC > ladoA);
     }
 }
