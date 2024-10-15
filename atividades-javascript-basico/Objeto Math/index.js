6c7f853b0e6068c44e0d619c5880a16e1094cd09
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-10-15 14:03:56 -0300

    Aula Numbers JS

---
+++ atividades-javascript-basico/Objeto Math/index.js
@@ -0,0 +1,18 @@
+console.log(100/0); // Dado Infinity
+
+//let num1 = 9;
+//console.log(num1 ** 0.5);
+
+
+// let num2= Math.floor(num1);
+// let num2 = Math.cell(num1);
+// let num2 = Math.round(num1);
+
+
+//console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8));
+//console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500, 9, 8));
+
+//const aleatorio = Math.round (Math.random() * (10 - 5) + 5);
+//console.log (aleatorio);
+
+// console.log(Math.PI);
