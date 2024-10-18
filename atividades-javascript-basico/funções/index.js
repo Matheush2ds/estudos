bec0f2c670bd2e9b014070b2e5ed5677ea485d7c
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-10-18 15:22:53 -0300

    Aprendendo o básico de funções em JS

---
+++ atividades-javascript-basico/funções/index.js
@@ -0,0 +1,39 @@
+function saudacao(nome) {
+    return(`Bom dia ${nome}!`);
+};
+
+const variavel = saudacao('Matheus');
+console.log(variavel); 
+
+//------------------------
+
+function soma (x, y) {
+    const resultado = x + y;
+    return resultado; // Nada que será colocado após no return ira aparecer
+
+};
+
+const resultado = soma (52,25);
+console.log (resultado);// não tem como modificar ou mexe o que tem dentro da função quando esta do lado de fora */
+
+
+//------------------------
+
+const raiz = function (n) { //Raiz é igual a uma função 
+    return n ** 0.5;
+
+};
+
+console.log (raiz(9));
+console.log (raiz(16));
+console.log (raiz(25));
+
+//------ Arrow function
+
+const arrowraiz = (n) => {
+    return n ** 0.5;
+
+};
+
+console.log (raiz(81));
+console.log (raiz(625));
