0b0e31ba55f0659c62002b92103739b910681e97
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-10-18 14:43:11 -0300

    Aprendendo sobre arrays no javascript

---
+++ atividades-javascript-basico/arrays/index.js
@@ -0,0 +1,30 @@
+// indices         0         1        2
+const alunos = ['Marcos', 'Maicon', 'João'];
+
+// console.log(alunos); imrpime os indices
+
+// alunos[0] = 'Eduardo'; Mudando indice 0 
+// alunos[3] = 'Luiza'; Adicionando indice
+// console.log(alunos [2]); imprimir nome do indice 2
+
+// alunos[alunos.length] = 'Luiza'; Adiciona mais 1 e o tamanho aumenta
+// alunos.push ('Otávio'); -- Outra maneira de adicionar ao indice (melhor maneira)
+// console.log (alunos.length); -- Tamanho do array
+
+// alunos.unshift('Luiza'); -- agora Luiza será o indice 0
+// alunos.unshift('Fábio'); -- agora Fabio será o indice 0 no lugar da luiza
+
+// alunos.pop(); -- Remove o elemento final do indice
+// const removido = alunos.shift(); salve o elemento que foi removido
+// console.log (removido); imprime com o elemento removido
+
+// delete alunos [1]; -- A partir daqui o indice 1 será vazio
+
+// console.log(alunos.slice (0,3)); -- Fatiar indice
+
+// console.log(typeof alunos); -- Para saber qual o tipo é
+
+
+
+
+
