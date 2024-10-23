5846b06dcb48796213f5603367b3b27aaeda0b0b
Matheus Henrique <97759985+Matheush2ds@users.noreply.github.com>
2024-10-23 18:11:05 -0300

    Aprendendo Operadores de Comparação

---
+++ atividades-javascript-basico/Operadores Comparação/index.js
@@ -0,0 +1,30 @@
+/*
+OPERADORES DE COMPARAÇÃO
+
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igualdade (valor) Não é recomendado a usar
+=== Igualdade estrita (valor e tipo)
+!= Diferente (valor) Não é recomendado a usar
+!== Diferente estrito  (valor e tipo)
+ */
+
+const comp = 10 > 5; //true
+console.log (comp);
+
+const comp1 = 10 >= 10; //true
+console.log (comp1);
+
+const comp2 =  9 < 10; //true
+console.log (comp2);
+
+const comp3 =  9 <= 9; //true
+console.log (comp3);
+
+const num1 = 9;
+const num2 = 18;
+
+const Igualdade = num1 == num2;
+console.log(Igualdade);
